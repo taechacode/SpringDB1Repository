da1188b5ddfd3f72db7ea47c1af8f9f8d849d376
taechacode <kangtaechan2015@gmail.com>
2024-05-01 19:29:00 +0900

    study: section1 데이터베이스 연결

---
+++ jdbc/src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -0,0 +1,24 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Slf4j
+public class DBConnectionUtil {
+
+    public static Connection getConnection() {
+        try {
+            // 데이터베이스에 연결하려면 JDBC가 제공하는 DriverManager의 getConnection() 사용.
+            // 라이브러리에 있는 데이터베이스 드라이버를 찾아서 해당 드라이버가 제공하는 커넥션을 반환.
+            Connection connection = DriverManager.getConnection(ConnectionConst.URL, ConnectionConst.USERNAME, ConnectionConst.PASSWORD);
+            log.info("get connection={}, class={}", connection, connection.getClass());
+            return connection;
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+}
