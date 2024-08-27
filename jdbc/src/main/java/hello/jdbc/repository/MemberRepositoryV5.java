ab6fd87d82d5ff7959e0bea99007db2b1e7e72d8
taechacode <kangtaechan2015@gmail.com>
2024-08-27 21:50:08 +0900

    study: section7 JDBC 반복 문제 해결 - JdbcTemplate

---
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV5.java
@@ -0,0 +1,56 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+
+/**
+ * JdbcTemplate 사용
+ */
+@Slf4j
+public class MemberRepositoryV5 implements MemberRepository {
+
+    private final JdbcTemplate template;
+
+    public MemberRepositoryV5(DataSource dataSource) {
+        this.template = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "INSERT INTO MEMBER(MEMBER_ID, MONEY) VALUES(?, ?)";
+        template.update(sql, member.getMemberId(), member.getMoney());
+        return member;
+    }
+
+    @Override
+    public Member findById(String memberId) {
+        String sql = "SELECT * FROM MEMBER WHERE MEMBER_ID = ?";
+        return template.queryForObject(sql, memberRowMapper(), memberId);
+    }
+
+    @Override
+    public void update(String memberId, int money) {
+        String sql = "UPDATE MEMBER SET MONEY = ? WHERE MEMBER_ID = ?";
+        template.update(sql, money, memberId);
+    }
+
+    @Override
+    public void delete(String memberId) {
+        String sql = "DELETE FROM MEMBER WHERE MEMBER_ID = ?";
+        template.update(sql, memberId);
+    }
+
+    private RowMapper<Member> memberRowMapper() {
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setMemberId(rs.getString("member_id"));
+            member.setMoney(rs.getInt("money"));
+            return member;
+        };
+    }
+
+}
